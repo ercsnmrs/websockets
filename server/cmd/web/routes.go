c2808c9ab45ca41313febe560cb7e6f18f902076
Ericson Muros <ericsonmuros@gmail.com>
2025-06-22 03:28:29 +0800

    chore: replace websocket connection to reconnecting one.

--- server/cmd/web/routes.go
+++ server/cmd/web/routes.go
@@ -11,5 +11,7 @@ func routes() http.Handler {
 	mux := pat.New()
 	mux.Get("/", http.HandlerFunc(handlers.Home))
 	mux.Get("/ws", http.HandlerFunc(handlers.WSEndpoint))
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/", http.StripPrefix("/static/", fileServer))
 	return mux
 }
